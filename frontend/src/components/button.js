648e21b66e1fa46952e63eb7697df62c3dce1305
Niall <nialldbarber@gmail.com>
2019-08-07 08:56:17 +0100

    Added styled components & button component

--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Btn } from '../styles/components/button'
 
 export default function Button({ text, icon, removeToast }) {
   return (
-    <button className="btn" type="submit" handleClick={removeToast}>
+    <Btn className="btn" type="submit" handleClick={removeToast}>
       <span>
         <span>{text}</span>
         {icon ? <img className="close-btn" src={icon} alt="close" /> : ''}
       </span>
-    </button>
+    </Btn>
   );
 }
 
