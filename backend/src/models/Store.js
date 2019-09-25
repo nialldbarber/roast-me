889d8bbdb35a51a5a39da914ef0b2949fa8964e8
Niall <nialldbarber@gmail.com>
2019-09-25 20:29:33 +0100

    Updated store model

--- backend/src/models/Store.js
+++ backend/src/models/Store.js
@@ -1,43 +1,44 @@
 import { Schema, model } from 'mongoose'
 
 const storeSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  slug: {
-    type: String,
-    unique: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  created: {
-    type: Date,
-    default: Date.now,
-  },
-  rating: {
-    type: Number,
-  },
-  // location: {
-  // 	type: {
-  // 		type: String,
-  // 		default: 'Point'
-  // 	},
-  // 	coordinates: [
-  // 		{
-  // 			type: Number,
-  // 			required: true
-  // 		}
-  // 	],
-  // 	address: {
-  // 		type: String,
-  // 		required: true
-  // 	}
-  // }
+	name: {
+		type: String,
+		trim: true,
+		required: true,
+		unique: true
+	},
+	location: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	description: {
+		type: String,
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	rating: {
+		type: Number
+	}
+	// location: {
+	// 	type: {
+	// 		type: String,
+	// 		default: 'Point'
+	// 	},
+	// 	coordinates: [
+	// 		{
+	// 			type: Number,
+	// 			required: true
+	// 		}
+	// 	],
+	// 	address: {
+	// 		type: String,
+	// 		required: true
+	// 	}
+	// }
 })
 
 export const Store = model('Store', storeSchema)
