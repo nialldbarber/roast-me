c76bfe42c8c7739395b7154aa6266c6ff774676e
Niall <nialldbarber@gmail.com>
2019-09-25 19:33:23 +0100

    Updated read me

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,63 +1,59 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: ['airbnb/base', 'eslint:recommended', 'plugin:react/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      ecmaVersion: 6,
-      jsx: true,
-      experimentalObjectRestSpread: true
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: ['react'],
-  rules: {
-    'function-paren-newline': 0,
-    semi: ["error", "never"],
-    "no-trailing-spaces": "error",
-    "no-console": "off",
-    indent: 0,
-    'no-use-before-define': 0,
-    'no-useless-escape': 0,
-    'class-methods-use-this': 0,
-    'arrow-parens': 0,
-    'no-underscore-dangle': 0,
-    'max-len': 'off',
-    'padded-blocks': 0,
-    camelcase: 0,
-    'no-unused-vars': [1, { vars: 'local', args: 'after-used' }],
-    'comma-dangle': 1,
-    'spaced-comment': 0,
-    'default-case': 0,
-    'no-param-reassign': 0,
-    'space-infix-ops': 1,
-    radix: 0,
-    'no-else-return': 0,
-    'no-return-assign': 0,
-    'space-before-function-paren': 0,
-    'no-await-in-loop': 0,
-    'no-restricted-syntax': [
-      2,
-      'DebuggerStatement',
-      'LabeledStatement',
-      'WithStatement'
-    ],
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: [ 'airbnb/base', 'eslint:recommended', 'plugin:react/recommended' ],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaFeatures: {
+			ecmaVersion: 6,
+			jsx: true,
+			experimentalObjectRestSpread: true
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	plugins: [ 'react' ],
+	rules: {
+		allowSingleLine: true,
+		'function-paren-newline': 1,
+		semi: [ 'error', 'never' ],
+		'no-trailing-spaces': 'error',
+		'no-console': 'off',
+		indent: 0,
+		'no-use-before-define': 0,
+		'no-useless-escape': 0,
+		'class-methods-use-this': 0,
+		'arrow-parens': 0,
+		'no-underscore-dangle': 0,
+		'max-len': 'off',
+		'padded-blocks': 0,
+		camelcase: 0,
+		'no-unused-vars': [ 1, { vars: 'local', args: 'after-used' } ],
+		'comma-dangle': 1,
+		'spaced-comment': 0,
+		'default-case': 0,
+		'no-param-reassign': 0,
+		'space-infix-ops': 1,
+		radix: 0,
+		'no-else-return': 0,
+		'no-return-assign': 0,
+		'space-before-function-paren': 0,
+		'no-await-in-loop': 0,
+		'no-restricted-syntax': [ 2, 'DebuggerStatement', 'LabeledStatement', 'WithStatement' ],
 
-    'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0,
-    'import/imports-first': 1,
-    'import/extensions': 0,
-    'import/prefer-default-export': 0,
-    'object-curly-newline': 0,
-    'object-curly-spacing': ["error", "always"],
-    curly: ['error', 'all']
-  }
-};
+		'import/no-unresolved': 0,
+		'import/no-extraneous-dependencies': 0,
+		'import/imports-first': 1,
+		'import/extensions': 0,
+		'import/prefer-default-export': 0,
+		'object-curly-newline': 1,
+		'object-curly-spacing': [ 'error', 'always' ],
+		curly: 2
+	}
+}
--- frontend/src/pages/store-list/store-list.js
+++ frontend/src/pages/store-list/store-list.js
@@ -7,25 +7,26 @@ import Loading from '~@components/loading'
 const StoreList = () => {
 	const { loading, error, data } = useQuery(GET_STORES)
 
-	if (loading) {return <Loading />}
-	if (error) {return `Error! ${error.message}`}
+	if (loading) return <Loading />
+
+	if (error) return `Error! ${error.message}`
 
 	console.log(data)
 
 	return (
 		<div className="container">
-		<h2 className="title">Store List</h2>
+			<h2 className="title">Store List</h2>
 			<section className="cards">
 				{data.getAllStores.map(({ name, _id }) => (
 					<div className="card" key={_id}>
 						<Link to={`/store/${_id}`}>
 							<div className="inner" name={name}>
-                <h3>{name}</h3>
-              </div>
+								<h3>{name}</h3>
+							</div>
 						</Link>
 					</div>
 				))}
-				</section>
+			</section>
 		</div>
 	)
 }
