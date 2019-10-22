92fbba951e74b6de2d2eecddbb23fa1c6e2e4454
Niall <nialldbarber@gmail.com>
2019-10-22 11:24:16 +0100

    Added login resolver

--- backend/src/schema/resolvers/user.js
+++ backend/src/schema/resolvers/user.js
@@ -1,15 +1,61 @@
 import { UserInputError } from 'apollo-server-express'
-import { hash } from 'bcryptjs'
+import { hash, compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 import { User } from '~@models/User'
+import { validateRegisterInput, validateLoginInput } from '~@utils/validation'
 
 require('dotenv').config({ path: '.env' })
 
+const generateToken = (user) => {
+	return sign(
+		{
+			id: user.id,
+			email: user.email,
+			username: user.username
+		},
+		process.env.JWT_SECRET,
+		{ expiresIn: '1h' }
+	)
+}
+
 export const user = {
 	Mutation: {
+		userLogin: async (_, { username, password }) => {
+			const { valid, errors } = validateLoginInput(username, password)
+
+			if (!valid) {
+				throw new UserInputError('Errors', { errors })
+			}
+
+			const user = await User.findOne({ username })
+
+			if (!user) {
+				errors.general = 'User not found'
+				throw new UserInputError('User not found', { errors })
+			}
+
+			const match = await compare(password, user.password)
+
+			if (!match) {
+				errors.general = 'Wrong credentials'
+				throw new UserInputError('Wrong credentials', { errors })
+			}
+
+			const token = generateToken(user)
+
+			return {
+				...user._doc,
+				id: user._id,
+				token
+			}
+		},
 		registerUser: async (_, { registerInput: { username, email, password, confirmPassword } }) => {
-			// TODO: validate user data
-			// TODO: make sure user doesnt exist already
+			const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+
+			if (!valid) {
+				throw new UserInputError('Errors', { errors })
+			}
+
 			const user = await User.findOne({ username })
 
 			if (user) {
@@ -19,7 +65,7 @@ export const user = {
 					}
 				})
 			}
-			// hash password and create an auth token
+
 			password = await hash(password, 12)
 
 			const newUser = new User({
@@ -31,15 +77,7 @@ export const user = {
 
 			const res = await newUser.save()
 
-			const token = sign(
-				{
-					id: res.id,
-					email: res.email,
-					username: res.username
-				},
-				process.env.JWT_SECRET,
-				{ expiresIn: '1h' }
-			)
+			const token = generateToken(res)
 
 			return {
 				...res._doc,
--- backend/src/utils/validation.js
+++ backend/src/utils/validation.js
@@ -1,16 +1,20 @@
 export const validateRegisterInput = (username, email, password, confirmPassword) => {
 	const errors = {}
+
 	if (username.trim() === '') {
 		errors.username = 'Username must not be empty'
 	}
+
 	if (email.trim() === '') {
 		errors.email = 'Email must not be empty'
 	} else {
 		const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+
 		if (!email.match(regEx)) {
 			errors.email = 'Email must be a valid email address'
 		}
 	}
+
 	if (password === '') {
 		errors.password = 'Password must not empty'
 	} else if (password !== confirmPassword) {
@@ -25,9 +29,11 @@ export const validateRegisterInput = (username, email, password, confirmPassword
 
 export const validateLoginInput = (username, password) => {
 	const errors = {}
+
 	if (username.trim() === '') {
 		errors.username = 'Username must not be empty'
 	}
+
 	if (password.trim() === '') {
 		errors.password = 'Password must not be empty'
 	}
