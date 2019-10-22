f65b0167200a936d3d5a913a228765a4ebf2506f
Niall <nialldbarber@gmail.com>
2019-10-22 16:17:21 +0100

    Typescriptified rating

--- client/src/components/rating/index.tsx
+++ client/src/components/rating/index.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
+import React, { FC, useState } from 'react'
 import { RatingContainer } from '~@styles/components/rating'
 
-const Rating = () => {
-  const [rating, setRating] = useState([
-    { name: 'Excellent', score: 0 },
-    { name: 'V.good', score: 0 },
-    { name: 'Average', score: 0 },
-    { name: 'Poor', score: 0 },
-    { name: 'Terrible', score: 0 },
-  ])
+const Rating: FC = () => {
+	const [ rating, setRating ] = useState([
+		{ name: 'Excellent', score: 0 },
+		{ name: 'V.good', score: 0 },
+		{ name: 'Average', score: 0 },
+		{ name: 'Poor', score: 0 },
+		{ name: 'Terrible', score: 0 }
+	])
 
-  return (
-    <RatingContainer>
-      {rating.map(({ name, score }, i) => (
-        <label key={i} htmlFor={name}>
-          {name}
-          <input type="checkbox" id={name} />
-        </label>
-      ))}
-    </RatingContainer>
-  )
+	return (
+		<RatingContainer>
+			{rating.map(({ name, score }, i) => (
+				<label key={i} htmlFor={name}>
+					{name}
+					<input type="checkbox" id={name} />
+				</label>
+			))}
+		</RatingContainer>
+	)
 }
 
 export default Rating
