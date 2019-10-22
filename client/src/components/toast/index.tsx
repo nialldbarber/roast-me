e29c35c98d655e322dc7b4930a361cb828aa10e2
Niall <nialldbarber@gmail.com>
2019-10-22 16:21:58 +0100

    Tidied up toast component

--- client/src/components/toast/index.tsx
+++ client/src/components/toast/index.tsx
@@ -6,16 +6,11 @@ import { Props } from '~@components/toast/types'
 const Toast: FC<Props> = ({ error, message, link }) => {
 	const [ active, setActive ] = useState(true)
 
-	const handleClick = () => {
-		setActive(false)
-		console.log('clicked!')
-	}
-
 	return (
 		<div className={`toast ${error && active ? 'active' : ''} toast-${error}`}>
 			<p>
 				<span>{message}</span>
-				<Button className="toaster" icon={close} removeToast={handleClick} />
+				<Button className="toaster" icon={close} removeToast={() => setActive(false)} />
 			</p>
 		</div>
 	)
