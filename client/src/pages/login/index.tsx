7bfc1defe41cf152939de15e99a3b4fad23a86de
Niall Barber <niall.barber@actual-experience.com>
2019-10-23 15:54:58 +0100

    Adding register to form

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,15 +1,34 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Form from '~@components/form'
+import Register from '~@components/register'
 import { Wrapper } from '~@pages/login/styles'
 
 const Login: FC = () => {
-	const options = [ 'username', 'password' ]
+	const [visibility, setVisibility] = useState({ signIn: true, register: false })
+	const options = ['username', 'password']
+
+	const handleVisibility = (e: any) => {
+		const target = e.target.name
+		if (target === 'signIn') setVisibility({ signIn: true, register: false })
+		if (target === 'register') setVisibility({ signIn: false, register: true })
+	}
+
+	const { signIn, register } = visibility
+
 	return (
 		<div className="container">
 			<h1>Sign in or create an account</h1>
+			<div>
+				<button onClick={handleVisibility} name="signIn">
+					Sign In
+				</button>
+				<button onClick={handleVisibility} name="register">
+					Register
+				</button>
+			</div>
 			<Wrapper>
-				<Form title="Sign In" options={options} />
-				<Form title="Register" options={options} />
+				<Form title="Sign In" options={options} visibility={signIn} />
+				<Register title="Register" visibility={register} />
 			</Wrapper>
 		</div>
 	)
