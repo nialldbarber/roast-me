90c648f514f5246587d506f4ed7e60b9406177fb
Niall <nialldbarber@gmail.com>
2019-10-25 12:22:40 +0100

    Tidied up front end directory structure

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,18 @@
+import { LOGIN, LOGOUT } from '~@constants/auth'
+
+export const authReducer = (state, action) => {
+	switch (action.type) {
+		case LOGIN:
+			return {
+				...state,
+				user: action.userData
+			}
+		case LOGOUT:
+			return {
+				...state,
+				user: null
+			}
+		default:
+			return state
+	}
+}
--- client/src/state/auth.js
+++ client/src/state/auth.js
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from 'react'
 import { LOGIN, LOGOUT } from '~@constants/auth'
+import { authReducer } from '~@reducers/auth'
 
 const initialState = {
 	user: null
@@ -11,37 +12,15 @@ const AuthContext = createContext({
 	logout: () => {}
 })
 
-const authReducer = (state, action) => {
-	switch (action.type) {
-		case LOGIN:
-			return {
-				...state,
-				user: action.payload
-			}
-		case LOGOUT:
-			return {
-				...state,
-				user: null
-			}
-		default:
-			return state
-	}
-}
-
 const AuthProvider = (props) => {
 	const [ state, dispatch ] = useReducer(authReducer, initialState)
 
 	const login = (userData) => {
-		dispatch({
-			type: LOGIN,
-			payload: userData
-		})
+		dispatch({ type: LOGIN, userData })
 	}
 
 	const logout = () => {
-		dispatch({
-			type: LOGOUT
-		})
+		dispatch({ type: LOGOUT })
 	}
 
 	return <AuthContext.Provider value={{ user: state.user, login, logout }} {...props} />
