a2848d2185727b4a1a76cdcd1c59cbe6c2058854
Niall <nialldbarber@gmail.com>
2019-10-25 12:41:57 +0100

    Added welcome message and logout button

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,37 +1,59 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import SVG from 'react-inlinesvg'
+import { AuthContext } from '~@state/auth'
 import logo from '~@assets/images/logo.svg'
 
-const Navbar: FC = () => (
-	<div className="navbar">
-		<header>
-			<div className="logo">
-				<Link to="/">
-					<SVG src={logo} />
-				</Link>
-			</div>
-		</header>
-		<nav>
-			<ul>
-				<li>
-					<NavLink exact to="/" activeClassName="active">
-						Stores
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/add-store" activeClassName="active">
-						Add Store
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/login" activeClassName="active">
-						Login
-					</NavLink>
-				</li>
-			</ul>
-		</nav>
-	</div>
-)
+const Navbar: FC = () => {
+	const { user, logout } = useContext(AuthContext)
+
+	return (
+		<div className="navbar">
+			<header>
+				<div className="logo">
+					<Link to="/">
+						<SVG src={logo} />
+					</Link>
+				</div>
+			</header>
+			<nav>
+				<ul>
+					{user ? (
+						<li>
+							<NavLink to="#" className="inactive">
+								Hey, {user.username}!
+							</NavLink>
+						</li>
+					) : (
+						''
+					)}
+					<li>
+						<NavLink exact to="/" activeClassName="active">
+							Stores
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/add-store" activeClassName="active">
+							Add Store
+						</NavLink>
+					</li>
+					{user ? (
+						<li>
+							<NavLink to="/login" activeClassName="active" onClick={logout}>
+								Logout
+							</NavLink>
+						</li>
+					) : (
+						<li>
+							<NavLink to="/login" activeClassName="active">
+								Login
+							</NavLink>
+						</li>
+					)}
+				</ul>
+			</nav>
+		</div>
+	)
+}
 
 export default Navbar
