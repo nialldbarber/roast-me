28555a6ce9113ff72646f903fd440ece76575be7
Niall <nialldbarber@gmail.com>
2019-10-25 12:49:55 +0100

    Persisted login with local storage

--- client/src/state/auth.js
+++ client/src/state/auth.js
@@ -1,9 +1,20 @@
 import React, { createContext, useReducer } from 'react'
+import jwtDecode from 'jwt-decode'
 import { LOGIN, LOGOUT } from '~@constants/auth'
 import { authReducer } from '~@reducers/auth'
 
 const initialState = {
 	user: null
+}
+
+if (localStorage.getItem('jwtToken')) {
+	const decodedToken = jwtDecode(localStorage.getItem('jwtToken'))
+
+	if (decodedToken.exp * 1000 < Date.now()) {
+		localStorage.removeItem('jwtToken')
+	} else {
+		initialState.user = decodedToken
+	}
 }
 
 const AuthContext = createContext({
@@ -16,10 +27,12 @@ const AuthProvider = (props) => {
 	const [ state, dispatch ] = useReducer(authReducer, initialState)
 
 	const login = (userData) => {
+		localStorage.setItem('jwtToken', userData.token)
 		dispatch({ type: LOGIN, userData })
 	}
 
 	const logout = () => {
+		localStorage.removeItem('jwtToken')
 		dispatch({ type: LOGOUT })
 	}
 
