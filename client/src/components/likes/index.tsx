a9269f83a42af8bebc2c0541b0e2d313ce31c476
Niall Barber <niall.barber@actual-experience.com>
2019-10-28 14:55:27 +0000

    Got like button working

--- client/src/components/likes/index.tsx
+++ client/src/components/likes/index.tsx
@@ -2,20 +2,26 @@ import React, { FC } from 'react'
 import { useMutation } from '@apollo/react-hooks'
 // Components
 import Button from '~@components/button'
+import Loading from '~@components/loading'
 // Types
 import { Props } from '~@components/likes/types'
 // Schema
-import { LIKE_STORE } from './schema'
+import { LIKE_STORE } from '~@components/likes/schema'
+import { GET_STORES } from '~@pages/all-stores/schema'
 
 const Likes: FC<Props> = ({ likes, id }) => {
-
-	const [likeStore] = useMutation(LIKE_STORE, {
-		variables: { _id: id }
+	const [likeStore, { loading, error }] = useMutation(LIKE_STORE, {
+		variables: { _id: id },
+		refetchQueries: [{ query: GET_STORES }]
 	})
+
+	if (error) console.log(`Error: ${error}`)
+	if (loading) return <Loading />
 
 	return (
 		<div>
 			<p>Likes</p>
+			<h1>{likes.length}</h1>
 			<Button text="Like" action={likeStore} />
 		</div>
 	)
