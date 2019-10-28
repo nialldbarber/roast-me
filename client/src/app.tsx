6d2947fd44cb2e14936132db7a985bbcfccea343
Niall Barber <niall.barber@actual-experience.com>
2019-10-28 10:59:03 +0000

    Moved state components to index

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,8 @@
 import React, { FC } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { ApolloProvider } from 'react-apollo'
 import { ThemeProvider } from 'styled-components'
 import ScrollToTop from 'react-router-scroll-top'
-import { client } from '~@utils/apollo'
-import { AuthProvider } from '~@state/auth'
+// Components
 import Navbar from '~@components/navbar'
 import AddStore from '~@pages/add-store'
 import AllStores from '~@pages/all-stores'
@@ -12,31 +10,28 @@ import IndividualStore from '~@pages/individual-store'
 import LoginRegister from '~@pages/login-register'
 import UserProfile from '~@pages/user-profile'
 import PageNotFound from '~@pages/page-not-found'
+// Styles
 import { theme } from '~@styles/utils/variables'
 import { GlobalStyle } from '~@styles/components/base'
 import '~@styles/sass/main.scss'
 
 const App: FC = () => (
-	<ApolloProvider client={client}>
-		<AuthProvider>
-			<ThemeProvider theme={theme}>
-				<GlobalStyle />
-				<Router>
-					<ScrollToTop>
-						<Navbar />
-						<Switch>
-							<Route exact path="/" component={AllStores} />
-							<Route path="/add-store" component={AddStore} />
-							<Route path="/profile" component={UserProfile} />
-							<Route path="/login" component={LoginRegister} />
-							<Route path="/store/:id" component={IndividualStore} />
-							<Route component={PageNotFound} />
-						</Switch>
-					</ScrollToTop>
-				</Router>
-			</ThemeProvider>
-		</AuthProvider>
-	</ApolloProvider>
+	<ThemeProvider theme={theme}>
+		<GlobalStyle />
+		<Router>
+			<ScrollToTop>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={AllStores} />
+					<Route path="/add-store" component={AddStore} />
+					<Route path="/profile" component={UserProfile} />
+					<Route path="/login" component={LoginRegister} />
+					<Route path="/store/:id" component={IndividualStore} />
+					<Route component={PageNotFound} />
+				</Switch>
+			</ScrollToTop>
+		</Router>
+	</ThemeProvider>
 )
 
 export default App
