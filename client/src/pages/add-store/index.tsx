2465d5122713d58ef97feb14843ff5f35b7486cd
Niall Barber <niall.barber@actual-experience.com>
2019-10-28 15:16:44 +0000

    Removed unused hook

--- client/src/pages/add-store/index.tsx
+++ client/src/pages/add-store/index.tsx
@@ -1,6 +1,4 @@
 import React, { FC } from 'react'
-// Hooks
-import useAddStore from '~@hooks/useAddStore'
 // Components
 import Toast from '~@components/toast'
 import AddStoreForm from '~@components/add-store'
@@ -11,14 +9,10 @@ import { Title } from '~@styles/components/title'
 import { Props } from '~@pages/add-store/types'
 
 const AddStore: FC<Props> = (props) => {
-	const { store, storeError } = useAddStore()
-	const { name } = store
-	const { status, message } = storeError
-
 	return (
 		<PageContainer>
 			<Title>Add Store</Title>
-			<Toast error={status} message={message} link={name} />
+			{/* <Toast error={status} message={message} link={name} /> */}
 			<AddStoreForm visibility={true} page={props} />
 		</PageContainer>
 	)
