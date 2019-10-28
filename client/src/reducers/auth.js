5742d6cdca5313a6792f50355fe97ee5663aa67c
Niall Barber <niall.barber@actual-experience.com>
2019-10-28 13:20:17 +0000

    Tidied auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,15 +3,9 @@ import { LOGIN, LOGOUT } from '~@constants/auth'
 export const authReducer = (state, action) => {
 	switch (action.type) {
 		case LOGIN:
-			return {
-				...state,
-				user: action.userData
-			}
+			return { ...state, user: action.userData }
 		case LOGOUT:
-			return {
-				...state,
-				user: null
-			}
+			return { ...state, user: null }
 		default:
 			return state
 	}
