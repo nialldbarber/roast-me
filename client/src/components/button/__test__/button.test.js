5c0fc5271051a1cad8188611c2f7d3a1ee2bcbfd
Niall Barber <niall.barber@actual-experience.com>
2019-10-29 14:32:21 +0000

    Updated button snapshot

--- client/src/components/button/__test__/button.test.js
+++ client/src/components/button/__test__/button.test.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import renderer from 'react-test-renderer'
 import Button from '~@components/button'
 
 describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Button />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  it('renders button text', () => {
+    const component = renderer.create(<Button text="Button text" />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
-  it('renders button correctly', () => {
-    const message = 'Button text'
-    const { queryByText } = render(<Button text={message} />)
-    expect(queryByText(message)).toBeInTheDocument()
+  it('renders button id when passed', () => {
+    const component = renderer.create(<Button id="button-id" />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
   it('matches snapshot', () => {
