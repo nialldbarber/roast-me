bf49cde5554a9360673c2ae616746a630c797f66
Niall Barber <niall.barber@actual-experience.com>
2019-10-29 14:08:15 +0000

    Added more tests to button

--- client/src/components/button/__test__/button.test.js
+++ client/src/components/button/__test__/button.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import renderer from 'react-test-renderer'
 import Button from '~@components/button'
 
 describe('<Button />', () => {
@@ -15,5 +16,11 @@ describe('<Button />', () => {
     const message = 'Button text'
     const { queryByText } = render(<Button text={message} />)
     expect(queryByText(message)).toBeInTheDocument()
+  })
+
+  it('matches snapshot', () => {
+    const message = 'Button text'
+    const tree = renderer.create(<Button text={message} />).toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
