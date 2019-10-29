101e0a838ae80debb488c156639fd02a950b6f1a
Niall Barber <niall.barber@actual-experience.com>
2019-10-29 16:01:00 +0000

    Updated button tests

--- client/src/components/button/__test__/button.test.js
+++ client/src/components/button/__test__/button.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { when } from 'jest-when'
 import '@testing-library/jest-dom/extend-expect'
 import renderer from 'react-test-renderer'
 import Button from '~@components/button'
@@ -13,6 +14,19 @@ describe('<Button />', () => {
   it('renders button id when passed', () => {
     const component = renderer.create(<Button id="button-id" />)
     const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('return true when function called', () => {
+    const fn = jest.fn()
+
+    when(fn).calledWith(true).mockReturnValue(true)
+
+    const component = renderer.create(<Button onClick={fn} />)
+    const tree = component.toJSON()
+    const result = fn(true)
+
+    expect(result).toEqual(true)
     expect(tree).toMatchSnapshot()
   })
 })
