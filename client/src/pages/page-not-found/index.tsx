52b29a906621c28297984bc4fce81fbee5ba242d
Niall <nialldbarber@gmail.com>
2019-11-02 08:01:55 +0000

    Added styles to page not found page

--- client/src/pages/page-not-found/index.tsx
+++ client/src/pages/page-not-found/index.tsx
@@ -4,13 +4,16 @@ import Button from '~@components/button'
 // Styles
 import { PageContainer } from '~@styles/components/container'
 import { Title } from '~@styles/components/title'
+import { Container } from '~@pages/page-not-found/styles'
 // Types
 import { Props } from '~@pages/page-not-found/types'
 
 const PageNotFound: FC<Props> = ({ history }) => (
 	<PageContainer>
 		<Title>This page does not exist!</Title>
-		<Button text="Return home?" action={() => history.push('/')} />
+		<Container>
+			<Button text="Return home?" action={() => history.push('/')} />
+		</Container>
 	</PageContainer>
 )
 
