3fe00e2f4942ec97c5f490dfbccc269a7a8f641e
Niall <nialldbarber@gmail.com>
2019-11-03 20:42:40 +0000

    Made sure refetch of user queries works properly

--- client/src/pages/all-users/schema.ts
+++ client/src/pages/all-users/schema.ts
@@ -1,11 +1,16 @@
 import { gql } from 'apollo-boost'
 
 export const GET_ALL_USERS = gql`
-  {
-    getAllUsers {
-      _id
-      username
-      email
-    }
-  }
+	{
+		getAllUsers {
+			_id
+			username
+			email
+			storesAdded {
+				_id
+				location
+				name
+			}
+		}
+	}
 `
