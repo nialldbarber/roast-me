158db86ab323a948853c2dd79b50d3e07880000a
Niall <nialldbarber@gmail.com>
2019-11-04 19:06:45 +0000

    Gave badges more descriptive names whilst I make the actual svg badges

--- client/src/utils/user.ts
+++ client/src/utils/user.ts
@@ -1,16 +1,16 @@
 export const getUserBadgeFromStoresAdded = (storesAdded: number): string => {
-  if (storesAdded < 0) return ''
-  if (storesAdded >= 1 && storesAdded <= 5) {
-    return 'its between 1 and 5'
-  } else if (storesAdded >= 6 && storesAdded <= 15) {
-    return 'its between 6 and 15'
-  } else if (storesAdded >= 16 && storesAdded <= 25) {
-    return 'its between 16 and 25'
-  } else if (storesAdded >= 26 && storesAdded <= 35) {
-    return 'its between 26 and 35'
-  } else if (storesAdded >= 36 && storesAdded <= 45) {
-    return 'its between 36 and 45'
-  } else {
-    return ''
-  }
+	if (storesAdded < 0) return ''
+	if (storesAdded >= 1 && storesAdded <= 5) {
+		return 'Badge Tier 1'
+	} else if (storesAdded >= 6 && storesAdded <= 15) {
+		return 'Badge Tier 2'
+	} else if (storesAdded >= 16 && storesAdded <= 25) {
+		return 'Badge Tier 3'
+	} else if (storesAdded >= 26 && storesAdded <= 35) {
+		return 'Badge Tier 4'
+	} else if (storesAdded >= 36 && storesAdded <= 45) {
+		return 'Badge Tier 5'
+	} else {
+		return ''
+	}
 }
