1240353e4be4b5b3d5f70daead5eb028d90b393c
Niall Barber <niall.barber@actual-experience.com>
2019-11-04 12:37:14 +0000

    Added spinner keyframes

--- client/src/styles/utils/keyframes.js
+++ client/src/styles/utils/keyframes.js
@@ -27,3 +27,24 @@ export const bounceFromTop = keyframes`
 		transform: translateX(-50%);
 	}
 `
+
+export const rotateSpinner = keyframes`
+	100% {
+		transform: rotate(360deg);
+	}
+`
+
+export const dashSpinner = keyframes`
+	0% {
+		stroke-dasharray: 1, 150;
+		stroke-dashoffset: 0;
+	}
+	50% {
+		stroke-dasharray: 90, 150;
+		stroke-dashoffset: -35;
+	}
+	100% {
+		stroke-dasharray: 90, 150;
+		stroke-dashoffset: -124;
+	}
+`
