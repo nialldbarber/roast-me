d5f81a3dd6a9f7f8114acfd0cc61a7e8c5c2f6b7
Niall Barber <niall.barber@actual-experience.com>
2019-11-04 12:48:48 +0000

    Updated spinner sass code

--- client/src/components/loading/styles.js
+++ client/src/components/loading/styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { rotateSpinner, dashSpinner } from '~@styles/utils/keyframes'
+import { centerMixin } from '~@styles/utils/mixins'
 
 export const Container = styled.div`
 	position: fixed;
@@ -8,4 +10,21 @@ export const Container = styled.div`
 	bottom: 0;
 	right: 0;
 	z-index: 9;
+`
+
+export const SpinnerContainer = styled.div`
+	position: fixed;
+	${centerMixin};
+`
+
+export const StyledSpinner = styled.svg`
+  animation: ${rotateSpinner} 1s linear infinite;
+  width: 70px;
+  height: 70px;
+
+  & .path {
+    stroke: ${(props) => props.theme.white};
+    stroke-linecap: round;
+    animation: ${dashSpinner} 2s ease-in-out infinite;
+  }
 `
