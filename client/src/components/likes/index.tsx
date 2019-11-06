cdb92a47c6b3ba309ebb0d9868931994afd832ca
Niall Barber <niall.barber@actual-experience.com>
2019-11-06 13:25:39 +0000

    Optional chaining for likes component

--- client/src/components/likes/index.tsx
+++ client/src/components/likes/index.tsx
@@ -15,22 +15,22 @@ import { GET_STORES } from '~@pages/all-stores/schema'
 import { Props } from '~@components/likes/types'
 
 const Likes: FC<Props> = ({ likes, id }) => {
-	const [ liked, setLiked ] = useState<boolean>(false)
+	const [liked, setLiked] = useState<boolean>(false)
 	const { user } = useContext(AuthContext)
-	const [ likeStore, { loading, error } ] = useMutation(LIKE_STORE, {
+	const [likeStore, { loading, error }] = useMutation(LIKE_STORE, {
 		variables: { _id: id },
-		refetchQueries: [ { query: GET_STORES } ]
+		refetchQueries: [{ query: GET_STORES }]
 	})
 
 	useEffect(
 		() => {
-			if (user && likes && likes.find((like) => like.username === user.username)) {
+			if (user && likes ?.find((like) => like.username === user.username)) {
 				setLiked(true)
 			} else {
 				setLiked(false)
 			}
 		},
-		[ user, likes ]
+		[user, likes]
 	)
 
 	if (error) console.log(`Error: ${error}`)
