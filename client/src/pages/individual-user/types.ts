8da571755aec254b29af201d4a4a31c5e3d02b57
Niall Barber <niall.barber@actual-experience.com>
2019-11-06 13:31:47 +0000

    Added types for stores added & removed unused packages

--- client/src/pages/individual-user/types.ts
+++ client/src/pages/individual-user/types.ts
@@ -2,3 +2,15 @@ export interface Props {
   match: any
   history: any
 }
+
+export interface StoresProps {
+  _id: string,
+  name: string,
+  likes: Array<LikeProps>
+}
+
+export interface LikeProps {
+  _id: string,
+  username: string,
+  createdAt: string
+}
