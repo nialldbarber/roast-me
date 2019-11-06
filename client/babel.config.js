24df1c9a91ea3d745b14e4e33d8092c74ed66ebf
Niall Barber <niall.barber@actual-experience.com>
2019-11-06 12:53:49 +0000

    Trying out optional chaining & nullish coalescing

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,8 +2,10 @@ module.exports = (api) => {
 	api.cache(true)
 
 	return {
-		presets: [ '@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react' ],
+		presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
 		plugins: [
+			'@babel/plugin-proposal-optional-chaining',
+			'@babel/plugin-proposal-nullish-coalescing-operator',
 			'@babel/proposal-class-properties',
 			'@babel/proposal-object-rest-spread',
 			[
