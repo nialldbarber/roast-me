a16c4f00a9b14e04b644e1a7402e97b43e6554df
Niall <nialldbarber@gmail.com>
2019-11-08 15:37:30 +0000

    Attempting rating resolver

--- server/src/schema/resolvers/rating.js
+++ server/src/schema/resolvers/rating.js
@@ -4,44 +4,61 @@ import { User } from '~@models/User'
 import { checkAuth } from '~@utils/auth'
 
 export const rating = {
-  Mutation: {
-    rateStore: async (_, { _id, taste, variety, atmosphere, wifi, seating, restroomQuality }, context) => {
-      // TODOS: 
-      // Make sure that a user can only rate a shop once
+	Mutation: {
+		rateStore: async (_, { _id, taste, variety, atmosphere, wifi, seating, restroomQuality }, context) => {
+			// TODOS:
+			// Make sure that a user can only rate a shop once
 
-      const { username } = checkAuth(context)
+			const { username } = checkAuth(context)
 
-      const getStore = await Store.findById(_id)
-      const getUser = await User.find({})
+			const getStore = await Store.findById(_id)
+			const getUser = await User.find({})
 
-      const getUserId = getUser.filter((user) => user.username === username)
-      const userId = getUserId[0]._id
+			const getUserId = getUser.filter((user) => user.username === username)
+			const userId = getUserId[0]._id
 
-      console.log(userId)
-      console.log('---------------------')
-      console.log(getStore.ratingAverage)
+			// console.log(userId)
+			// console.log('---------------------')
+			// console.log(getStore.ratingAverage)
 
-      if (getStore) {
-        if (getStore.ratingAverage.find(user => user._id === userId)) {
-          getStore.ratingAverage.unshift({
-            taste,
-            variety,
-            atmosphere,
-            wifi,
-            seating,
-            restroomQuality,
-            userId: userId,
-          })
-        }
-      } else {
-        throw new UserInputError('Store not found')
-      }
+			//console.log(getStore.ratingAverage.filter((store) => store.userId === userId))
 
-      await getStore.save()
-      return getStore
-    }
-  }
+			if (getStore.ratingAverage.find((person) => person._id === userId)) {
+				console.log('it exists on the DB')
+				throw new UserInputError('Store not found')
+			} else {
+				console.log('it doesnt exist on the DB')
+				getStore.ratingAverage.unshift({
+					taste,
+					variety,
+					atmosphere,
+					wifi,
+					seating,
+					restroomQuality,
+					userId: userId
+				})
+			}
+
+			// if (getStore) {
+			// 	if (getStore.ratingAverage.filter((user) => user.userId === userId)) {
+			// 		getStore.ratingAverage.unshift({
+			// 			taste,
+			// 			variety,
+			// 			atmosphere,
+			// 			wifi,
+			// 			seating,
+			// 			restroomQuality,
+			// 			userId: userId
+			// 		})
+			// 	}
+			// } else {
+			// 	throw new UserInputError('Store not found')
+			// }
+
+			await getStore.save()
+			return getStore
+		}
+	}
 }
 
-
-// loop through 
+// loop through
