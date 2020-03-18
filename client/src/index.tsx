65004bdcb3ee38c5f531c510996234043be9e726
Niall Barber <niall.barber@actual-experience.com>
2020-03-18 17:42:00 +0000

    Destructured createRoot to reduce code

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
 import { AuthProvider } from '~/state/auth'
 import { client } from '~/utils/apollo'
 import App from '~/app'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
 	<ApolloProvider client={client}>
 		<AuthProvider>
 			<App />
